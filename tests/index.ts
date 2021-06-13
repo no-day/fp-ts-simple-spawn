c22f8312d85f4931b31e99c4f9803490334af506
Michael Bock <me@thought2.de>
2021-06-13 05:46:21 +0200

    Fix test

--- tests/index.ts
+++ tests/index.ts
@@ -4,13 +4,27 @@ import * as E from 'fp-ts/Either'
 import * as T from 'fp-ts/Task'
 
 describe('spawn', () => {
-  it('prints to stdout', () =>
+  // it('prints to stdout', () =>
+  //   pipe(
+  //     $.spawn('echo', ['one', 'two']),
+  //     T.map((result) =>
+  //       expect(result).toStrictEqual(
+  //         E.right({
+  //           stdout: 'one two\n',
+  //           stderr: '',
+  //         })
+  //       )
+  //     ),
+  //     (p) => p()
+  //   ))
+
+  it('takes stdin', () =>
     pipe(
-      $.spawn('yarn', ['-v']),
+      $.spawn('cat', [], { stdin: 'Hello' }),
       T.map((result) =>
         expect(result).toStrictEqual(
           E.right({
-            stdout: 'one two\n',
+            stdout: 'Hello',
             stderr: '',
           })
         )
@@ -26,6 +40,79 @@ describe('spawn', () => {
           E.left({
             _tag: 'CommandNotFoundError',
             command: 'somethingUnknown',
+          })
+        )
+      ),
+      (p) => p()
+    ))
+
+  it('throws exit code error', () =>
+    pipe(
+      $.spawn('yarn', ['somethingUnknown']),
+      T.map((result) =>
+        expect(result).toMatchObject(
+          E.left({
+            _tag: 'ExitCodeError',
+            exitCode: 1,
+          })
+        )
+      ),
+      (p) => p()
+    ))
+})
+
+describe('spawnWithExitCode', () => {
+  // it('prints to stdout', () =>
+  //   pipe(
+  //     $.spawnWithExitCode('echo', ['one', 'two']),
+  //     T.map((result) =>
+  //       expect(result).toStrictEqual(
+  //         E.right({
+  //           stdout: 'one two\n',
+  //           stderr: '',
+  //           exitCode: 0,
+  //         })
+  //       )
+  //     ),
+  //     (p) => p()
+  //   ))
+
+  it('takes stdin', () =>
+    pipe(
+      $.spawnWithExitCode('cat', [], { stdin: 'Hello' }),
+      T.map((result) =>
+        expect(result).toStrictEqual(
+          E.right({
+            stdout: 'Hello',
+            stderr: '',
+            exitCode: 0,
+          })
+        )
+      ),
+      (p) => p()
+    ))
+
+  it('throws with unknown command', () =>
+    pipe(
+      $.spawnWithExitCode('somethingUnknown', ['one', 'two']),
+      T.map((result) =>
+        expect(result).toMatchObject(
+          E.left({
+            _tag: 'CommandNotFoundError',
+            command: 'somethingUnknown',
+          })
+        )
+      ),
+      (p) => p()
+    ))
+
+  it('returns exit code', () =>
+    pipe(
+      $.spawnWithExitCode('yarn', ['somethingUnknown']),
+      T.map((result) =>
+        expect(result).toMatchObject(
+          E.right({
+            exitCode: 1,
           })
         )
       ),
